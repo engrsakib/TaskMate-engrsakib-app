63296940b8eb976cf11c9345662e93ceb7563f48
engrsakib <snazmussakib01@gmail.com>
2025-05-01 01:39:13 +0600

    middleware.ts file reconize for protectings private routes

--- task-management/src/app/auth/login/page.tsx
+++ task-management/src/app/auth/login/page.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import task from "@/assets/images/task.png";
 import Image from "next/image";
 import LogInFrom from "@/component/auth/LogInFrom";
+import Head from "next/head";
+
 const Login = () => {
   return (
-    <div className="grid grid-cols-12">
+   <>
+    <Head>
+        <title>LogIn | MyApp</title>
+        <meta name="description" content="Learn more about our company." />
+      </Head>
+
+     <div className="grid grid-cols-12">
       {/* left side */}
       <div className="w-full lg:h-screen lg:col-span-5 col-span-12 flex justify-center items-center bg-linear-120 from-[rgb(45,102,86)] to-[rgb(5,6,18)]">
         <Image src={task} alt="task" className="object-cover mx-auto"/>
@@ -14,6 +22,9 @@ const Login = () => {
         <LogInFrom></LogInFrom>
       </div>
     </div>
+   
+   
+   </>
   );
 };
 
