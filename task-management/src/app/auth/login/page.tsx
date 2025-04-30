663aedbe0db841556b806a86507f5d74f676cb71
engrsakib <snazmussakib01@gmail.com>
2025-04-30 23:05:16 +0600

    auth/login page define and liniar background fully complete

---
+++ task-management/src/app/auth/login/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='grid grid-cols-12'>
+      {/* left side */}
+        <div className='w-full h-screen col-span-5 bg-linear-120 from-[rgb(45,102,86)] to-[rgb(5,6,18)]'>
+
+        </div>
+      {/* right side */}
+      <div className='col-span-7'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Login;
