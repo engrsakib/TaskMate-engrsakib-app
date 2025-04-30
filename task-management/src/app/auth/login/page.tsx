f478cafb7dba8a99ba7dbcc8062a469a9998aebf
engrsakib <snazmussakib01@gmail.com>
2025-05-01 00:25:45 +0600

    input field initialize and make login from with full responsive

--- task-management/src/app/auth/login/page.tsx
+++ task-management/src/app/auth/login/page.tsx
@@ -6,11 +6,11 @@ const Login = () => {
   return (
     <div className="grid grid-cols-12">
       {/* left side */}
-      <div className="w-full h-screen col-span-5 flex justify-center items-center bg-linear-120 from-[rgb(45,102,86)] to-[rgb(5,6,18)]">
+      <div className="w-full lg:h-screen lg:col-span-5 col-span-12 flex justify-center items-center bg-linear-120 from-[rgb(45,102,86)] to-[rgb(5,6,18)]">
         <Image src={task} alt="task" className="object-cover mx-auto"/>
       </div>
       {/* right side */}
-      <div className="col-span-7 flex justify-center items-center bg-white">
+      <div className="lg:col-span-7 w-10/12 mx-auto col-span-12 flex justify-center items-center bg-white">
         <LogInFrom></LogInFrom>
       </div>
     </div>
--- task-management/src/component/auth/LogInFrom.tsx
+++ task-management/src/component/auth/LogInFrom.tsx
@@ -4,7 +4,30 @@ import React from 'react'
 function LogInFrom() {
   return (
     <div>
-      <h1 className='text-[40px] font-600 text-black'>LogIn</h1>
+      {/* text */}
+        <div>
+        <h1 className='text-[40px] font-600 text-black text-center'>LogIn</h1>
+        <h2 className='text-gray-600 capitalize '>WelcomeBack Please Enter Your Details to LogIn </h2>
+        </div>
+
+        {/* from */}
+        <form>
+            <div className='flex flex-col gap-4 mt-10'>
+                <input type="text" placeholder='Email' className='border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500'/>
+                <input type="password" placeholder='Password' className='border border-gray-300 rounded-md p-2 focus:outline-none focus:border-blue-500'/>
+            </div>
+    
+            <div className='flex justify-between mt-4'>
+                <div className='flex items-center gap-2'>
+                <input type="checkbox" id="remember-me" className='w-4 h-4'/>
+                <label htmlFor="remember-me" className='text-gray-600'>Remember Me</label>
+                </div>
+                <a href="#" className='text-blue-500'>Forgot Password?</a>
+            </div>
+    
+            <button type="submit" className='btn btn-primary text-white py-2 px-4 rounded-md mt-6 w-full'>LogIn</button>
+        </form>
+
     </div>
   )
 }
