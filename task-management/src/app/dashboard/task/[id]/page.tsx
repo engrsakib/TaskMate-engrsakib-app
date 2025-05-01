e7f1fd7105ca6895bff0fb6d7a9dfa37329cb3ab
engrsakib <snazmussakib01@gmail.com>
2025-05-01 22:05:03 +0600

    dynamic rotes define for presenting tasks details

---
+++ task-management/src/app/dashboard/task/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='bg-white w-[90%] mx-auto h-screen border-transparent rounded-2xl drop-shadow-2xl'>
+      
+    </div>
+  )
+}
